d7fb1a8bb0a73e1774661fc4f67e9fac1418be8f
Ivan Tustanivsky <itustanivsky@ninevastudios.com>
2018-11-05 13:02:21 +0200

    add ios goodies plugin reference to project

--- Source/iOSGoodiesTest/iOSGoodiesTest.Build.cs
+++ Source/iOSGoodiesTest/iOSGoodiesTest.Build.cs
@@ -8,7 +8,7 @@ public class iOSGoodiesTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "IOSGoodies" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
